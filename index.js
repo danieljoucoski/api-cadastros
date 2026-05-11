0c33e442dc8cf85d8ca316cd972b26633b9a014d
Ryancabral11 <cabral.ryan@escola.pr.gov.br>
2026-05-11 08:21:44 -0300

    2c

--- index.js
+++ index.js
@@ -1,48 +1,64 @@
 const express = require("express");
 const fs = require("fs");
 
-const app = express ();
+const app = express();
 const port = 3000;
 
 app.use(express.json());
 
 
 //http://localhost:3000/saudacao?nome=bruno
-app.get("/saudacao",(req,res)=>{
+app.get("/saudacao", (req, res) => {
     const nome = req.query.nome;
-    
-    if(!nome){
+
+    if (!nome) {
         return res.status(404).json(
             {
-                erro:"nome não foi informado"
+                erro: "nome não foi informado"
             }
         )
     }
 
-res.json(
-    {
-        mensagem: `saudacões ${nome}!`
-    }
-)
+    res.json(
+        {
+            mensagem: `saudacões ${nome}!`
+        }
+    )
 
 
 })
-app.post("/imc",(req,res)=>{
-    const {nome, idade, altura,peso}= req.body;
-    if(!nome && !idade && !altura && !peso){
+app.post("/imc", (req, res) => {
+    const { nome, idade, altura, peso } = req.body;
+    if (!nome && !idade && !altura && !peso) {
         return res.status(404).json({ erro: "dados incompleto" })
     }
-const imc = peso / (altura * altura);
+    const imc = peso / (altura * altura);
 
-res.json({
-    nome,
-    idade,
-    imc:imc.toFixed(2)
+
+
+    res.json({
+        nome,
+        idade,
+        imc: imc.toFixed(2)
+    })
+
 })
 
+app.post("/media", (req, res) => {
+    const { nota1, nota2 } = req.body;
+    if (!nota1 && !nota2) {
+        return res.status(404).json({ erro: "dados incompleto" })
+    }
+    const media = (parseFloat(nota1) + parseFloat(nota2)) / 2;
+
+    res.json({
+        nota1,
+        nota2,
+        media: parseFloat(media)
+    })
 })
 
 //finalzão
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`servidor rodando em http://localhost:${port}`)
 })
